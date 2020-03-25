82fe59e3fb361c72a62bd812b7ad5f5db9286019
Brian Stewart <brian.stewart@jamf.com>
2020-03-25 15:03:39 -0500

    Update gitignore

--- src/test/groovy/com/thecodesmith/controller/BadgeControllerSpec.groovy
+++ src/test/groovy/com/thecodesmith/controller/BadgeControllerSpec.groovy
@@ -1,10 +1,10 @@
 package com.thecodesmith.controller
 
 import io.micronaut.context.ApplicationContext
-import io.micronaut.runtime.server.EmbeddedServer
-import io.micronaut.http.client.RxHttpClient
 import io.micronaut.http.HttpResponse
 import io.micronaut.http.HttpStatus
+import io.micronaut.http.client.RxHttpClient
+import io.micronaut.runtime.server.EmbeddedServer
 import spock.lang.AutoCleanup
 import spock.lang.Shared
 import spock.lang.Specification
@@ -14,10 +14,9 @@ class BadgeControllerSpec extends Specification {
     @Shared @AutoCleanup EmbeddedServer embeddedServer = ApplicationContext.run(EmbeddedServer)
     @Shared @AutoCleanup RxHttpClient client = embeddedServer.applicationContext.createBean(RxHttpClient, embeddedServer.getURL())
 
-
     void "test index"() {
         given:
-        HttpResponse response = client.toBlocking().exchange("/badge")
+        HttpResponse response = client.toBlocking().exchange('/badge')
 
         expect:
         response.status == HttpStatus.OK
