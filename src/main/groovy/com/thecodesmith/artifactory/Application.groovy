6491c1be8e48f2671e0b12804b33bbb7c2f42939
Brian Stewart <brian.stewart@jamf.com>
2020-04-13 20:13:49 -0500

    Enable finer logging

--- src/main/groovy/com/thecodesmith/artifactory/Application.groovy
+++ src/main/groovy/com/thecodesmith/artifactory/Application.groovy
@@ -1,11 +1,25 @@
 package com.thecodesmith.artifactory
 
+import groovy.util.logging.Slf4j
+import io.micronaut.context.annotation.Value
+import io.micronaut.context.event.ApplicationEventListener
+import io.micronaut.discovery.event.ServiceStartedEvent
 import io.micronaut.runtime.Micronaut
 import groovy.transform.CompileStatic
 
+@Slf4j
 @CompileStatic
-class Application {
+class Application implements ApplicationEventListener<ServiceStartedEvent> {
+
+    @Value('${artifactory.host}')
+    String artifactoryHost
+
     static void main(String[] args) {
         Micronaut.run(Application)
+    }
+
+    @Override
+    void onApplicationEvent(ServiceStartedEvent event) {
+        log.info "Serving badges for Artifactory at $artifactoryHost"
     }
 }
