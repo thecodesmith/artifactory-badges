4411a3974995f02af76a08540c0ca530c8d5b8c8
Brian Stewart <brian.stewart@jamf.com>
2020-04-22 15:40:26 -0500

    Add /config endpoint

--- src/main/groovy/com/thecodesmith/artifactory/controller/RootController.groovy
+++ src/main/groovy/com/thecodesmith/artifactory/controller/RootController.groovy
@@ -1,16 +1,26 @@
 package com.thecodesmith.artifactory.controller
 
+import com.thecodesmith.artifactory.config.ArtifactoryConfig
 import groovy.transform.CompileStatic
 import io.micronaut.http.HttpResponse
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
 
+import javax.inject.Inject
+
 @CompileStatic
 @Controller('/')
 class RootController {
 
+    @Inject ArtifactoryConfig config
+
     @Get('/')
     HttpResponse index() {
         HttpResponse.ok(status: 200)
+    }
+
+    @Get('/config')
+    HttpResponse config() {
+        HttpResponse.ok(host: config.host, port: config.port)
     }
 }
