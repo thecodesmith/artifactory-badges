03916230cf2a26513af8e5110edff0ec3c0b04f3
Brian Stewart <brian.stewart@jamf.com>
2020-05-06 16:04:38 -0500

    Support dashes/underscores/spaces in version, as well as custom labels and colors

--- src/main/groovy/com/thecodesmith/artifactory/controller/BadgeController.groovy
+++ src/main/groovy/com/thecodesmith/artifactory/controller/BadgeController.groovy
@@ -4,11 +4,14 @@ import com.thecodesmith.artifactory.clients.ArtifactoryClient
 import com.thecodesmith.artifactory.services.BadgeService
 import groovy.transform.CompileStatic
 import groovy.util.logging.Slf4j
+import io.micronaut.context.annotation.Value
 import io.micronaut.http.HttpResponse
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
 import io.micronaut.http.annotation.Produces
+import io.micronaut.http.annotation.QueryValue
 
+import javax.annotation.Nullable
 import javax.inject.Inject
 
 import static com.thecodesmith.artifactory.util.Constants.VERSION_NOT_FOUND
@@ -18,12 +21,25 @@ import static com.thecodesmith.artifactory.util.Constants.VERSION_NOT_FOUND
 @Controller('/badge')
 class BadgeController {
 
+    static final String DEFAULT_LABEL = 'version'
+    static final String DEFAULT_COLOR = 'blue'
+
     @Inject ArtifactoryClient artifactoryClient
     @Inject BadgeService badgeService
 
+    @Value('${artifactory.host}')
+    String artifactoryHost
+
+    @Value('${shields.host}')
+    String shieldsHost
+
     @Get('/{repo}/{groupId}/{artifactId}')
     @Produces('image/svg+xml;charset=utf-8')
-    HttpResponse<String> getBadge(String repo, String groupId, String artifactId) {
+    HttpResponse<String> getBadge(String repo,
+                                  String groupId,
+                                  String artifactId,
+                                  @QueryValue Optional<String> label,
+                                  @QueryValue Optional<String> color) {
         try {
             log.info "Finding version for '$groupId:$artifactId' in $repo"
             def artifactoryStart = now()
@@ -32,23 +48,29 @@ class BadgeController {
 
             log.info "Latest version for '$repo/$groupId:$artifactId': $version"
             def shieldsStart = now()
-            def badge = badgeService.getVersionBadge(version)
+            def badge = badgeService.getBadge(label.orElse(DEFAULT_LABEL), version, color.orElse(DEFAULT_COLOR))
             def shieldsTime = now() - shieldsStart
 
-            log.info "Request performance: Artifactory: $artifactoryTime, shields.io: $shieldsTime"
+            log.info """|Request performance:
+                        |  ${formatResponseTime(artifactoryTime)} - Artifactory ($artifactoryHost)
+                        |  ${formatResponseTime(shieldsTime)} - Shields: ($shieldsHost)""".stripMargin()
             if (!badge) {
-                throw new RuntimeException('Unable to acquire badge from https://shields.io: response is empty')
+                throw new RuntimeException("Unable to acquire badge from $shieldsHost: response is empty")
             }
 
             log.info "Returning badge: $badge"
             return HttpResponse.ok(badge)
         } catch (e) {
             log.error('Unable to generate badge', e)
-            return HttpResponse.ok(badgeService.getNotFoundBadge())
+            return HttpResponse.ok(badgeService.errorBadge)
         }
     }
 
     protected static Long now() {
         new Date().time
+    }
+
+    protected static String formatResponseTime(Long duration) {
+        "$duration ms".padLeft(7)
     }
 }
--- src/main/groovy/com/thecodesmith/artifactory/services/BadgeService.groovy
+++ src/main/groovy/com/thecodesmith/artifactory/services/BadgeService.groovy
@@ -2,25 +2,47 @@ package com.thecodesmith.artifactory.services
 
 import com.thecodesmith.artifactory.clients.ShieldsIoClient
 import groovy.transform.CompileStatic
+import groovy.util.logging.Slf4j
+import io.micronaut.http.client.exceptions.HttpClientResponseException
 
 import javax.inject.Inject
 import javax.inject.Singleton
 
-@CompileStatic
+@Slf4j
 @Singleton
+@CompileStatic
 class BadgeService {
 
     @Inject ShieldsIoClient client
 
-    String getVersionBadge(String version) {
-        getBadge('version', version, 'blue')
+    String getBadge(String label, String message, String color) {
+        try {
+            client.getBadge(label, escaped(message), color)
+        } catch (HttpClientResponseException e) {
+            log.error "Response from Shields client: $e.status.code - $e.status.reason", e
+            if (e.response.body.present && isSvg(e.response.body() as String)) {
+                log.info 'Response body contains SVG, returning it'
+                return e.response.body()
+            }
+            errorBadge
+        }
     }
 
-    String getBadge(String label, String message, String color) {
-        client.getBadge(label, message, color)
+    String escaped(String version) {
+        version.replaceAll('-', '--')
+               .replaceAll('_', '__')
+               .replaceAll(' ', '_')
     }
 
     String getNotFoundBadge() {
-        this.getClass().getResourceAsStream('/not-found.svg').text
+        this.getClass().getResourceAsStream('/badges/not-found.svg').text
+    }
+
+    String getErrorBadge() {
+        this.getClass().getResourceAsStream('/badges/error.svg').text
+    }
+
+    boolean isSvg(String text) {
+        text.startsWith('<svg ')
     }
 }
